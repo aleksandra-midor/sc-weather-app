bb8e529837cad4bf1636b2f08622fe9dc4032519
octane\ola <ola_midor@outlook.com>
2020-09-02 10:33:19 +0200

    checking if the month already exist in the arrey

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const weather = [];
+let weather = [];
+// line below should be remover at the end and above LET weather changed to CONST 
+const weatherAdded = [{temperature: 04, date:'2020-03-15'},{temperature:10, date:'2020-04-15'},{temperature:28, date:'2020-07-15'},{temperature:02, date:'2020-11-15'}];
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM has been loaded")
+    weather = weather.concat(weatherAdded)
+    console.log("DOM has been loaded",weather)
 });
 
 
@@ -10,19 +13,44 @@ document.getElementById('add').addEventListener('click', function(){
     
     var temp = parseInt(document.querySelector('#temp').value);
     var date = document.querySelector('#date').value;
-
+    //var existingMonth = date
+    
     
     if (Number.isInteger(temp) === true && date !== '') {
+        
         var dayWeather = {
             temperature: temp,
             date: date
         }
-        weather.push(dayWeather)
+        var dateSplited = date.split('-') 
+
+        
+        var foundIndex = weather.findIndex ( x => {
+            // console.log(x)
+
+            var xDateSplited = x.date.split('-')
+            // console.log(xDateSplited[1])
+            // if (xDateSplited[1] !== dateSplited[1]) {
+                //     return false
+                // } 
+                return xDateSplited[1] === dateSplited[1]
+            }) 
+            console.log(foundIndex)
+            if (foundIndex === -1) {
+                weather.push(dayWeather)
+        }
+
+
     }  else {
         alert ('Please input a number')
     }
     console.log(weather)
 })
+
+
+//var maxTemp = Math.max(...weather.temperature);
+//console.log(maxTemp)
+
 
 
 
