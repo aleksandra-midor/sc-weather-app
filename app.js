5581e84b76a80fe42710a055eb352856a77c9639
octane\ola <ola_midor@outlook.com>
2020-09-03 13:04:23 +0200

    seeding data changed

--- app.js
+++ app.js
@@ -47,20 +47,20 @@ document.addEventListener('DOMContentLoaded', function() {
             temperature: temp,
             date: date
         }
-        var dateSplited = date.split('-') 
-
-        var foundIndex = weather.findIndex ( x => {
-            var xDateSplited = x.date.split('-')
-                return xDateSplited[1] === dateSplited[1]
-            }) 
-            console.log(foundIndex)
-            if (foundIndex === -1) {
-                weather.push(dayWeather)
-        }
+        
+        weather.push(dayWeather)
+        // var dateSplited = date.split('-') 
+        // var foundIndex = weather.findIndex ( x => {
+        //     var xDateSplited = x.date.split('-')
+        //         return xDateSplited[1] === dateSplited[1]
+        //     }) 
+        //     console.log(foundIndex)
+        //     if (foundIndex === -1) {
+        //     }
     }  else {
-        alert ('Please input a number as a date and choose a date')
+      alert ('Please input a number as a date and choose a date')
     }
-
+          
     displayTable()
     console.log(weather)
 }
@@ -71,7 +71,8 @@ function generateTemp() {
   for (var i = 1; i < 13; i++) {
     var newDayTemp = {
       temperature: Math.floor(Math.random() * 50) ,
-      date: '2020-' + i + '-15'
+      date: randomDate()
+
     }
     weather.push(newDayTemp);
   }
@@ -108,3 +109,44 @@ function lowestTemp() {
   }
   console.log('Min temperature is' + lowTemp);
 }
+
+
+// new function
+
+function randomDate() {
+  var start = new Date(2020, 0, 1)
+  var end = new Date(2020,11,31)
+  var dateObj = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  console.log(end.getTime())
+
+  var month = dateObj.getMonth() + 1;
+  var day = dateObj.getDate();
+  var year = dateObj.getFullYear();
+  
+  var newDate = year + '-' + month + '-' + day;
+  return newDate
+}
+console.log(randomDate());
+
+
+
+
+
+function lowestTemp() {
+  var lowTemp =100;
+  const weatherTemp = weather.map( dayWeather => dayWeather.temperature);
+  console.log(weatherTemp);
+
+  for (var i = 0; i < weatherTemp.length; i++) {
+    if (lowTemp > weatherTemp[i]) {
+      lowTemp = weatherTemp[i]
+    }
+  }
+  console.log('Min temperature is' + lowTemp);
+}
+
+
+
+
+
+
